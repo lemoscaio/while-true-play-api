f804cf00e84e9b9b369395fb0656c16cb2db20b6
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-05-10 03:38:42 -0300

    heroku setup

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get("/", (req, res) => {
+    res.send("Online")
+})
+
 app.listen(process.env.PORT || 5000, () =>
     console.log(`Server running on port ${process.env.PORT || 5000}`)
 )
