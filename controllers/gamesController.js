b68644b64dee5cf4422312bbdd4cdaebebb6bea2
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-05-10 23:41:16 -0300

    finished feat: getGameById

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,3 +1,34 @@
-import joi, { func } from "joi";
-
 import db from "./../db.js"
+
+export async function getGameById(req, res) {
+    // Converting ID to be recognized by collection
+    const id = parseInt(req.params.id)
+    // Destructuring collection
+    const gamesCollection = db.collection("games")
+    try {
+        // Checking if game exists
+        const game = await gamesCollection.findOne({ id: id })
+        if (game) {
+            // Raising view count
+            await gamesCollection.updateOne(
+                {
+                    id: id,
+                },
+                {
+                    $set: {
+                        views: game.views + 1,
+                    },
+                }
+            )
+
+            // Removing private information
+            delete game._id
+            res.status(200).send(game)
+        } else {
+            res.status(404).send()
+        }
+    } catch (e) {
+        console.log(e)
+        res.status(401).send()
+    }
+}
--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express"
 
-import { getGameById } from "../controllers/gamesController"
+import { getGameById } from "../controllers/gamesController.js"
 
 const gamesRouter = Router()
+
+gamesRouter.get("/games/:id", getGameById)
 
 export default gamesRouter
