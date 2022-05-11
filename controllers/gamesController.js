7998406fcff32d5e59c2dfd28d68d820e3943f0f
lemoscaio <caio_lemos94@hotmail.com>
2022-05-11 09:27:40 -0300

    added getAllGames function  on /games endpoint

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,5 +1,19 @@
 import db from "./../db.js"
 
+export async function getAllGames(req, res) {
+    try {
+        // Get games from database
+        const games = await db.collection("games").find({}).toArray()
+
+        // Return if doesn't exist
+        if (!games) return res.status(404).send("Couldn't find games")
+        // Return list of games
+        res.send(games)
+    } catch (error) {
+        res.status(500).send("Something went wrong retrieving the data")
+    }
+}
+
 export async function getGameById(req, res) {
     // Converting ID to be recognized by collection
     const id = parseInt(req.params.id)
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 // Routers
+app.get("/", (req, res) => {res.send("Online")})
 app.use(gamesRouter);
 
 app.listen(process.env.PORT || 5000, () =>
