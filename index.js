bbf8b2aa396ecd3a9a8499fb3629a623c1cd5ebb
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-05-10 22:56:24 -0300

    feat: GamesRouter

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 
+import gamesRouter from "./routes/gamesRouter.js";
+
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Online")
-})
+// Routers
+app.use(gamesRouter);
 
 app.listen(process.env.PORT || 5000, () =>
     console.log(`Server running on port ${process.env.PORT || 5000}`)
