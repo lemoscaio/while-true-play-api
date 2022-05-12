7fc6c877b9e3bb6d6e7fc95844c9d9439a13770e
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-05-12 18:32:24 -0300

    fix: added missing router

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json())
 app.get("/", (req, res) => {res.send("Online")})
 app.use(gamesRouter)
 app.use(usersRouter)
+app.use(checkoutRouter)
 
 app.listen(process.env.PORT || 5000, () =>
     console.log(`Server running on port ${process.env.PORT || 5000}`)
