ff8b9ef6d13f2b10aff4c6bb79650a65020752e3
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-05-12 00:05:09 -0300

    feat: sign in

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { signUp } from "./../controllers/usersController.js"
+import { signUp, signIn } from "./../controllers/usersController.js"
 import { validateSignUpInfo } from "./../middlewares/validateSignUpInfo.js"
+import { validateSignInInfo } from "./../middlewares/validateSignInInfo.js"
 
 const usersRouter = Router()
 
 usersRouter.post("/sign-up", validateSignUpInfo, signUp)
+usersRouter.post("/sign-in", validateSignInInfo, signIn)
 
 export default usersRouter
