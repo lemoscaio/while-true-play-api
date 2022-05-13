a44422ef3edb4e6a94c86f84fa313efb749cdc33
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-05-12 21:50:33 -0300

    feat: Send email to buyer

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -3,7 +3,7 @@ import db from "./../db.js"
 export async function buyGame(req, res) {
     try {
         // Destructuring request and renaming for practicity
-        const { games: newGames } = req.body
+        const { games: newGames, email } = req.body
         // Destructuring previous info
         const { user, session } = res.locals
 
@@ -38,6 +38,25 @@ export async function buyGame(req, res) {
             .updateOne({ _id: session.userId }, { $set: { games: totalGames } })
         // Finishing process
         console.log("New games bought and added successfully.")
+
+        const sgMail = require("@sendgrid/mail")
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+        const msg = {
+            to: email,
+            from: "patrickandradepatrick@hotmail.com", // Change to your verified sender
+            subject: "Purchase at While(true) Play Store",
+            text: "Thank you for buying at our store! We hope to see you again soon.",
+            html: "<strong>Thank you for buying at our store! We hope to see you again soon.</strong>",
+        }
+        sgMail
+            .send(msg)
+            .then(() => {
+                console.log("Email sent")
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+
         res.status(201).send()
     } catch (e) {
         console.log(e)
